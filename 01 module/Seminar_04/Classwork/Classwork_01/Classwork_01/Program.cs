fea04c94975759dc6bb1db00642ed652cdb8a0be
germandilio <glmikhaylov@edu.hse.ru>
2020-09-23 21:17:04 +0300

    First commit

---
+++ 01 module/Seminar_04/Classwork/Classwork_01/Classwork_01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Classwork_01
+{
+    class Program
+    {
+        static bool Triangle(double x, double y, double z, out double p, out double s)
+        {
+            bool flag = false;
+            p = (x + y + z);
+            s = 0;
+            if (x < y + z && y < x + z && z < x + y)
+            {
+                flag = true;
+                s = Math.Sqrt(p * (p / 2 - x) * (p / 2 - y) * (p / 2 - z));
+            }
+            else
+            {
+                flag = false;
+            }
+            return flag;
+        }
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Введите 3 числа(стороны треугольника) ");
+                double x, y, z;
+                while (!double.TryParse(Console.ReadLine(), out x) ||
+                       !double.TryParse(Console.ReadLine(), out y) ||
+                       !double.TryParse(Console.ReadLine(), out z))
+                {
+                    Console.WriteLine("Ошибка ввода, введите 3 числа(стороны треугольника) ");
+                }
+                double p, s;
+                Console.WriteLine(Program.Triangle(x, y, z, out p, out s));
+                Console.WriteLine(s);
+
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            
+
+
+
+        }
+    }
+}
