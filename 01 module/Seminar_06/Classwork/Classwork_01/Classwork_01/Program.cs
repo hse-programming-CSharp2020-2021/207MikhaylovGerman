b9fc6db986b5c8e3dcbdfd8e7a52c6bcaaf2b2fc
germandilio <glmikhaylov@edu.hse.ru>
2020-10-01 19:38:12 +0300

    First commit

---
+++ 01 module/Seminar_06/Classwork/Classwork_01/Classwork_01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Classwork_01
+{
+    class Program
+    {
+        static void ArrayFill(ref char[] array, string input)
+        {
+            int i = 0;
+            foreach (var item in input)
+            {
+                array[i] = item;
+                i ++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int N;
+            string input = Console.ReadLine();
+            if (!int.TryParse(input , out N) || N <= 0)
+            {
+                Console.WriteLine("Ошибка ввода");
+            }
+            char[] array = new char[N];
+            ArrayFill(ref array, input);
+            Array.ForEach(array, x => Console.Write($"{x} "));
+        }
+    }
+}
