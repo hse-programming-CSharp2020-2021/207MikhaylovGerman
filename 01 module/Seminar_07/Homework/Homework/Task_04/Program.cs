bd99b5996e9efa4e5b857ca0b7d9b12f04850330
germandilio <glmikhaylov@edu.hse.ru>
2020-10-05 22:11:41 +0300

    First commit

---
+++ 01 module/Seminar_07/Homework/Homework/Task_04/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Task_04
+{
+    class Program
+    {
+        /// <summary>
+        /// Метод вычисляет детерминант(определитель) матрицы 2x2 и 3x3.
+        /// </summary>
+        /// <param name="array">Массив, определитель которого надо вычислить</param>
+        /// <param name="N">Размер матрицы</param>
+        /// <returns></returns>
+        private static int Determenand(ref int[,] array,int N)
+        {
+            if (N == 2)
+            {
+                return (array[0, 0] * array[1, 1]) - (array[0, 1] * array[0, 1]);
+            }
+            else
+            {
+                return ((array[0, 0] * array[1, 1] * array[2, 2]) + (array[0, 1] * array[1, 2] * array[2, 0])
+                    + (array[0, 2] * array[1, 0] * array[2, 1])) - ((array[0, 0] * array[1, 2] * array[2, 1]) +
+                    (array[0, 1] * array[1, 0] * array[2, 2]) + (array[0, 2] * array[1, 1] * array[2, 0]));
+            }
+
+        }
+        private static void Main(string[] args)
+        {
+            // Для матрицы 2x2.
+            int N = 2;
+            int[,] array = new int[N, N];
+            InputArray(ref array, N);
+            int result = Determenand(ref array, N);
+            Console.WriteLine($"Определитель матрицы 2x2 = {result}");
+
+            // Для матрицы 3x3.
+            N = 3;
+            int[,] array1 = new int[N, N];
+            InputArray(ref array1, N);
+            int result1 = Determenand(ref array1, N);
+            Console.WriteLine($"Определитель матрицы 3x3 = {result1}");
+
+        }
+
+        /// <summary>
+        /// Ввод массива с клавиатуры.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="N"></param>
+        private static void InputArray(ref int[,] array, int N)
+        {
+            Console.WriteLine("Введите двумерный массив, используя в качестве разделителя ' ', ',', '.', ':', '\t' ");
+            for (int i = 0; i < N; i++)
+            {
+                for (int k = 0; k < N; k++)
+                {
+                    Console.WriteLine($"Введите элемент {i + 1} строки, {k + 1} столбца");
+                    while (!int.TryParse(Console.ReadLine(), out array[i, k]))
+                    {
+                        Console.WriteLine("Incorrect input");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 01 module/Seminar_07/Homework/Homework/Task_06/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Task_06
+{
+    class Program
+    {
+        /// <summary>
+        /// Метод вычисляет два определителя матрицы 3x6.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="result"></param>
+        private static void Determenand(int[,] array, out int[] result)
+        {
+            result = new int[2];
+            // Определитель первой половины матрицы.
+            result[0] = ((array[0, 0] * array[1, 1] * array[2, 2]) + (array[0, 1] * array[1, 2] * array[2, 0])
+                + (array[0, 2] * array[1, 0] * array[2, 1])) - ((array[0, 0] * array[1, 2] * array[2, 1]) +
+                (array[0, 1] * array[1, 0] * array[2, 2]) + (array[0, 2] * array[1, 1] * array[2, 0]));
+            // Определитель второй половины матрицы.
+            result[1] = ((array[0, 3] * array[1, 4] * array[2, 5]) + (array[0, 4] * array[1, 5] * array[2, 3])
+                + (array[0, 5] * array[1, 3] * array[2, 4])) - ((array[0, 3] * array[1, 5] * array[2, 4]) +
+                (array[0, 4] * array[1, 3] * array[2, 5]) + (array[0, 5] * array[1, 4] * array[2, 3]));
+        }
+
+    
+
+        private static void Main(string[] args)
+        {
+            // Заполнение массива.
+            int[,] Array1 = new int[3, 6];
+            FillRandomArray(ref Array1);
+            // Вычисление двух определителей.
+            int[] result;
+            Determenand(Array1, out result);
+
+            PrintArray(ref result);
+        }
+
+        /// <summary>
+        /// Метод печатает элементы на экран.
+        /// </summary>
+        private static void PrintArray(ref int[] result)
+        {
+            Console.WriteLine("Детерменанды(определители) ");
+            Array.ForEach(result, x => Console.Write(x + " "));
+        }
+
+        /// <summary>
+        /// Метод заполняет массив random числами от MinValue до MaxValue.
+        /// </summary>
+        private static void FillRandomArray(ref int[,] Array1)
+        {
+            Random random = new Random();
+            int MinValue = 0;
+            int MaxValue = 20;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int k = 0; k < 6; k++)
+                {
+                    Array1[i, k] = random.Next(MinValue, MaxValue);
+
+                }
+            }   
+        }
+    }
+}
