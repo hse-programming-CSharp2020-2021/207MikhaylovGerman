2f03528b6e1523635d5fe4baff453477b3d912e0
germandilio <70810123+germandilio@users.noreply.github.com>
2020-11-03 00:58:26 +0300

    First commit

---
+++ 02 module/Seminar_02/Classwork/Classwork/Task_02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_02
+{
+    class LatinChar
+    {
+        private char _char = 'a';
+
+        public int Char(char symbol)
+        {
+            _char = symbol;
+            return _char;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LatinChar latinChar = new LatinChar();
+
+            char minChar = char.Parse(Console.ReadLine());
+            char maxChar = char.Parse(Console.ReadLine());
+            for (char i = minChar; i < maxChar; i++)
+            {
+                Console.WriteLine((char)latinChar.Char(i)); 
+
+            }
+        }
+    }
+}
