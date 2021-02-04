7787541274ba48a38749fe982bc21db00606ab59
germandilio <70810123+germandilio@users.noreply.github.com>
2021-02-04 13:47:29 +0300

    First commit

---
+++ 03 module/Seminar_04/Classwork/ClassWork/ClassWork/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassWork
+{
+    class PlayIsStartedEvemtArgs : EventArgs
+    {
+
+    }
+
+    class BandMaster
+    {
+        public event EventHandler<PlayIsStartedEvemtArgs> PlayIsStartedEvent;
+
+
+        private void StartPlay(int number)
+        {
+        }
+    }
+    abstract class OrchestraPlayer
+    {
+        public string Name { get; set; }
+
+        public OrchestraPlayer(string name)
+        {
+            Name = name;
+        }
+        public OrchestraPlayer()
+        {
+        }
+
+        public abstract void PlayIsStartedEventHandler(object sender, PlayIsStartedEvemtArgs e);
+           
+    }
+
+    class Violinist : OrchestraPlayer
+    {
+        public override void PlayIsStartedEventHandler(object sender, PlayIsStartedEvemtArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = new Violinist() { Name = "dwdwd" };
+        }
+    }
+}
